9869d9b0ff08af5fc91f3a7097a712d0d6000a1a
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2025-01-21 20:49:15 +0200

    did printing

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -2,4 +2,10 @@
 print("Hello world!")
 # string manipulation series
 # we will print five lines of code using one print statement and \n to create new lines
-print("first line\nsecond line\nthird line\nfourth line\nfifth line")
+print("first line\nsecond line\nthird line\nfourth line\nfifth line\n")
+# first way to insert space between strings is concatenation
+print("Hello" " " + "world")
+# or
+print( "Hello" + " " + "world" )
+# second way to insert space between strings without the plus
+print("Hello " "world")
