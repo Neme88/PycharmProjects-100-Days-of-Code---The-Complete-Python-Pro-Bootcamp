a0490c304f344548cab3879697533d3852980d1b
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2025-01-21 19:49:39 +0200

    used th \n to add 5 new liines in one print statement instead of using 5 print statement or better known as string formatting

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,2 +1,5 @@
 # Write your code below this line 👇
 print("Hello world!")
+# string manipulation series
+# we will print five lines of code using one print statement and \n to create new lines
+print("first line\nsecond line\nthird line\nfourth line\nfifth line")
