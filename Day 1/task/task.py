92ccc666a4596a5b280e9f84b62a541073e55416
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2025-01-28 15:20:53 +0200

    added another approach to solving final day exercise code challenge

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -152,24 +152,24 @@ print(f"your bmi is: {round(bmi, 2)}")
 # print(f"Your name is {name}, you are {age}years old, you are tall true or false ? {isTall}.")
 # # final day project (build a tip calculator)
 # implementation 1
-print("Welcome to tip calculator!")
-bill_amount =float(input("What was the total bill? $ \n"))
-tip_amount = int(input("How much tip would you like to give? 10, 12, or 15? \n"))
-people = int(input("How many people to split the bill? \n"))
-total_bill = (tip_amount / 100) * 100 + bill_amount # which is 10/100 = 0.1 * 100 = 10 + 100 which is 110
-bill_per_person = total_bill / people
-each_person_total = round(bill_per_person, 2)
-print(f"Each person should pay ${each_person_total}")
-# Implementation two
 # print("Welcome to tip calculator!")
-# bill_amount = float(input("What is the total bill? $"))
-# tip_amount = int(input("How much tip would you like to give? 10, 12, or 15? "))
-# people = int(input("How many people to split the bill? "))
-# tip_percentage = tip_amount / 100
-# total_bill = bill_amount + tip_percentage
+# bill_amount =float(input("What was the total bill? $ \n"))
+# tip_amount = int(input("How much tip would you like to give? 10, 12, or 15? \n"))
+# people = int(input("How many people to split the bill? \n"))
+# total_bill = (tip_amount / 100) * 100 + bill_amount # which is 10/100 = 0.1 * 100 = 10 + 100 which is 110
 # bill_per_person = total_bill / people
-# final_bill = round(total_bill, 2 )
-# print(f"Each person should pay {final_bill}")
+# each_person_total = round(bill_per_person, 2)
+# print(f"Each person should pay ${each_person_total}")
+# # Implementation two
+print("Welcome to tip calculator!")
+bill_amount = float(input("What is the total bill? $"))
+tip_amount = int(input("How much tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill? "))
+tip_percentage = tip_amount / 100
+total_bill = bill_amount + tip_percentage
+bill_per_person = total_bill / people
+final_bill = round(total_bill, 2 )
+print(f"Each person should pay {final_bill}")
 
 
 
