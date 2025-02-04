9fa7d47769d82a24af57d37466b51e700786feb5
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2025-02-04 18:45:59 +0200

    working out how much customer need to pay if they want extra cheese  to their pizza depending on the size using example from the the tutor

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -302,12 +302,18 @@ elif size == 'M':
     bill += 20
 elif size == 'L':
     bill += 25
+else:
+    print("You typed the wrong input")
 # work out how much to add to their bill based on their pepperoni choice
 if pepperoni == 'Y':
     if size == 's':
         bill=+ 2
     else:
         bill =+ 3
+# work out their final amount based on whether if they want extra cheese
+if extra_cheese == 'Y':
+    bill+= 1
+print(f"Your final bill is ${bill}")
 
 
 
