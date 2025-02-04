96f8a279b70b80c324ad337a8ab1e4cdcda3ffbc
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2025-02-04 18:35:54 +0200

    working out how much customer need to pay using example from the the tutor

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -289,6 +289,23 @@ if ordering_pizza:
 else:
     print("Do you want to order a pizza")
 
+#  tutor solution
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L: ")
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+extra_cheese = input("Do you want extra cheese? Y or N: ")
+# work out how much they need to pay based on pizza size
+bill = 0
+if size == 'S':
+    bill+= 15
+elif size == 'M':
+    bill += 20
+elif size == 'L':
+    bill += 25
+
+
+
+
 
 
 
